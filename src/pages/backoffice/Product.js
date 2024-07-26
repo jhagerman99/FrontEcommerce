5da139817c8620d5fbd1d6ac6a2ec6ea07a264c4
jhagerman99 <106148658+jhagerman99@users.noreply.github.com>
2024-07-26 17:39:14 +0700

    ีupdate edit product

--- src/pages/backoffice/Product.js
+++ src/pages/backoffice/Product.js
@@ -6,8 +6,8 @@ import config from "../../config";
 import axios from "axios";
 
 function Product () {
-    const [product, setProduct] = useState({});
-    const [products, setProducts] = useState([]);
+    const [product, setProduct] = useState({}); //Create, Update
+    const [products, setProducts] = useState([]); //Show all
 
     useEffect(() => {
         fetchData();
@@ -19,7 +19,12 @@ function Product () {
             product.price = parseInt(product.price);
             product.cost = parseInt(product.cost);
 
-            const res = await axios.post(config.apiPath + '/product/create', product, config.headers());
+            let res;
+            if (product.id === undefined) {
+                res = await axios.post(config.apiPath + '/product/create', product, config.headers());
+            } else {
+               res = await axios.put(config.apiPath + '/product/update', product, config.headers());
+            }
 
             if(res.data.message === 'success') {
                 Swal.fire({
@@ -30,6 +35,8 @@ function Product () {
                 });
                 document.getElementById('modalProduct_btnClose').click();
                 fetchData();
+
+                setProduct({...product, id: undefined}); //Clear id
             }
         } catch (e) {
             console.log(e);
@@ -45,8 +52,8 @@ function Product () {
         try {
             const res = await axios.get(config.apiPath + '/product/list', config.headers());
 
-            if(res.data.result !== undefined){
-                setProduct(res.data.result);
+            if (res.data.results !== undefined) {
+                setProducts(res.data.results);
             }
         } catch (e) {
             Swal.fire({
@@ -64,12 +71,82 @@ function Product () {
             cost: ''
         })
     }
+
+    const handleRemove = async (item) => {
+        try {
+            const button = await Swal.fire({
+                text: 'remove item',
+                title: 'remove',
+                icon: 'question',
+                showCancelButton: true,
+                showConfirmButton: true
+            })
+
+            if (button.isConfirmed){
+                const res = await axios.delete(config.apiPath + '/product/remove/' + item.id, config.headers());
+
+                if (res.data.message === 'success') {
+                    Swal.fire({
+                        title: 'remove',
+                        text: 'remove success',
+                        icon: 'success',
+                        timer: 1000
+                    })
+
+                    fetchData();
+                }
+            }
+        } catch (e) {
+            Swal.fire({
+                title: 'error',
+                text: e.message,
+                icon:'error'
+            })
+        }
+    }
+
     return <BackOffice>
         <div className="h4">Product</div>
-        <button onClick={clearForm} className="btn btn-primary" data-toggle='modal' data-target='#modalProduct'>
+        <button onClick={clearForm} className="btn btn-primary mr-2" data-toggle='modal' data-target='#modalProduct'>
             <i className="fa fa-plus"></i>เพิ่มรายการ
         </button>
+
+        <button className="btn btn-success">
+            <i className="fa fa-arrow-down mr-2"></i>Import from Excel
+        </button>
         
+        <table className="mt-3 table table-bordered table-stripped">
+            <thead>
+                <tr>
+                    <th>name</th>
+                    <th width='150px' className="text-right">cost</th>
+                    <th width='140px' className="text-right">price</th>
+                    <th width='150px' className="text-center"></th>
+                </tr>
+            </thead>
+            <tbody>
+                {(products.length > 0) ? products.map(item =>
+                    <tr key={item.id}>
+                        <td>{item.name}</td>
+                        <td className="text-right">{item.cost}</td>
+                        <td className="text-right">{item.price}</td>
+                        <td className="text-center">
+                            <button className="btn btn-primary mr-2"
+                                data-toggle='modal'
+                                data-target='#modalProduct'
+                                onClick={e => setProduct(item)}
+                            >
+                                <i className="fa fa-edit"></i>
+                            </button>
+                            <button className="btn btn-danger" onClick={e => handleRemove(item)}>
+                                <i className="fa fa-times"></i>
+                            </button>
+                        </td>
+                    </tr>
+                ) : <>no product</>}
+            </tbody>
+        </table>
+
         <MyModal id='modalProduct' title='สินค้า'>
             <div>
                 <div>ชื่อสินค้า</div>
