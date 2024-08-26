bbad905295a0e66a65c6dbbb6c7ab23a8a133cf5
jhagerman99 <106148658+jhagerman99@users.noreply.github.com>
2024-08-26 17:05:19 +0700

    update dashboard

--- src/pages/backoffice/Dashboard.js
+++ src/pages/backoffice/Dashboard.js
@@ -1,45 +1,70 @@
 import BackOffice from "../../components/BackOffice";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, scales } from 'chart.js';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+import axios from "axios";
+import config from "../../config";
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 function DashBoard() {
-    const data = {
-        labels: ['January', 'February', 'March', 'April', 'May'],
+    const [data, setData] = useState(null); // เปลี่ยนให้เริ่มต้นเป็น null
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top'
+        },
+        title: {
+            display: true,
+            text: 'Monthly Sale Data'
+        },
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+}
+
+const fetchData = async () => {
+    const res = await axios.get(config.apiPath + '/api/sale/dashboard', config.headers());
+    let data = [];
+
+    if (res.data.results !== undefined) {
+        for (let i = 0; i < res.data.results.length; i++) {
+            data.push(res.data.results[i].sumPrice);
+        }
+    }
+
+    setData({
+        labels: ['มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฏาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม'],
         datasets: [
             {
                 label: 'Monthly Sales',
-                data: [10, 20, 30, 40, 50],
+                data: data,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
             }
         ]
-    }
+    });
+}
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top'
-            },
-            title: {
-                display: true,
-                text: 'Monthly Sale Data'
-            },
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
-        }
-    }
-
-    return <BackOffice>
-        <Bar data={data} options={options} style={{ width: '50%' }} />
-    </BackOffice>
+    return (
+        <BackOffice>
+            {data ? (
+                <Bar data={data} options={options} style={{ width: '50%' }} />
+            ) : (
+                <p>Loading...</p>
+            )}
+        </BackOffice>
+    );
 }
 
 export default DashBoard;
